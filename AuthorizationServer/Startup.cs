9cafc97a4fd1ad15e05c1f51ee9f38a908a9a2c4
vinayak <vnkakmt@gmail.com>
2023-05-17 16:20:41 +0530

    Add project files.

---
+++ AuthorizationServer/Startup.cs
@@ -0,0 +1,110 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<DbContext>(options =>
+            {
+                // Configure the context to use an in-memory store.
+                options.UseInMemoryDatabase(nameof(DbContext));
+
+                // Register the entity sets needed by OpenIddict.
+                options.UseOpenIddict();
+            });
+
+            services.AddOpenIddict()
+
+        // Register the OpenIddict core components.
+        .AddCore(options =>
+        {
+            // Configure OpenIddict to use the EF Core stores/models.
+            options.UseEntityFrameworkCore()
+                .UseDbContext<DbContext>();
+        })
+        // Register the OpenIddict server components.
+        .AddServer(options =>
+        {
+            options
+                .AllowClientCredentialsFlow();
+
+            options
+                .SetTokenEndpointUris("/connect/token");
+
+            // Encryption and signing of tokens
+            options
+                .AddEphemeralEncryptionKey()
+                .AddEphemeralSigningKey()
+                .DisableAccessTokenEncryption(); // remove this line so that OpenIddict encrypts the access token
+
+            // Register scopes (permissions)
+            options.RegisterScopes("api");
+
+            // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+            options
+                .UseAspNetCore()
+                .EnableTokenEndpointPassthrough();
+        });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    options.LoginPath = "/account/login";
+                });
+
+            services.AddHostedService<TestData>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
